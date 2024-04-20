74a7ec7ba78abfbf2e9c4dc70ac2c4a687a9de99
fankuan <fmkuan44@gmail.com>
2024-04-20 15:18:16 +0700

    .

--- algos/mf_nas.py
+++ algos/mf_nas.py
@@ -1,6 +1,9 @@
 from . import Algorithm, IteratedLocalSearch, RandomSearch, SuccessiveHalving
 import numpy as np
-
+import pathlib
+import pickle as p
+from models import Network
+root = pathlib.Path(__file__).parent.parent
 
 class MF_NAS(Algorithm):
     def __init__(self):
@@ -29,48 +32,53 @@ class MF_NAS(Algorithm):
     def search(self, **kwargs):
         # Stage 1: Training-free Search
         print('- Stage 1: Training-free Local Search')
-        if self.optimizer_stage1 == 'FLS':
-            optimizer_stage1 = IteratedLocalSearch(first_improvement=True)
-        elif self.optimizer_stage1 == 'BLS':
-            optimizer_stage1 = IteratedLocalSearch(first_improvement=False)
-        elif self.optimizer_stage1 == 'RS':
-            optimizer_stage1 = RandomSearch()
-        else:
-            raise ValueError(f'Not support this optimizer in MF-NAS framework: {self.optimizer_stage1}')
-        optimizer_stage1.adapt(self.problem)
-        optimizer_stage1.using_zc_metric = self.using_zc_metric_stage1
-
-        _ = optimizer_stage1.search(max_time=9999999999, max_eval=self.max_eval_stage1,
-                                    metric=self.metric_stage1, **kwargs)
-
-        self.total_time += optimizer_stage1.total_time
-        self.total_epoch += optimizer_stage1.total_epoch
-
-        network_history_stage1 = optimizer_stage1.network_history[:self.max_eval_stage1]
-        score_history_stage1 = optimizer_stage1.score_history[:self.max_eval_stage1]
-
-        ## Remove duplication
-        if self.problem.search_space.name == 'NB-101':
-            h_history_stage1 = []
-            for network in network_history_stage1:
-                h = self.problem.get_h(network.genotype)
-                h_history_stage1.append(h)
-            _, I = np.unique(h_history_stage1, return_index=True)
-
-        else:
-            genotype_history_stage1 = np.array([network.genotype for network in network_history_stage1])
-            _, I = np.unique(genotype_history_stage1, axis=0, return_index=True)
-        network_history_stage1 = np.array(network_history_stage1)[I]
-        score_history_stage1 = np.array(score_history_stage1)[I]
-
-        ## Sort
-        I = np.flip(np.argsort(score_history_stage1))
-        network_history_stage1 = network_history_stage1[I]
+        # if self.optimizer_stage1 == 'FLS':
+        #     optimizer_stage1 = IteratedLocalSearch(first_improvement=True)
+        # elif self.optimizer_stage1 == 'BLS':
+        #     optimizer_stage1 = IteratedLocalSearch(first_improvement=False)
+        # elif self.optimizer_stage1 == 'RS':
+        #     optimizer_stage1 = RandomSearch()
+        # else:
+        #     raise ValueError(f'Not support this optimizer in MF-NAS framework: {self.optimizer_stage1}')
+        # optimizer_stage1.adapt(self.problem)
+        # optimizer_stage1.using_zc_metric = self.using_zc_metric_stage1
+        #
+        # _ = optimizer_stage1.search(max_time=9999999999, max_eval=self.max_eval_stage1,
+        #                             metric=self.metric_stage1, **kwargs)
+        #
+        # self.total_time += optimizer_stage1.total_time
+        # self.total_epoch += optimizer_stage1.total_epoch
+        #
+        # network_history_stage1 = optimizer_stage1.network_history[:self.max_eval_stage1]
+        # score_history_stage1 = optimizer_stage1.score_history[:self.max_eval_stage1]
+        #
+        # ## Remove duplication
+        # if self.problem.search_space.name == 'NB-101':
+        #     h_history_stage1 = []
+        #     for network in network_history_stage1:
+        #         h = self.problem.get_h(network.genotype)
+        #         h_history_stage1.append(h)
+        #     _, I = np.unique(h_history_stage1, return_index=True)
+        #
+        # else:
+        #     genotype_history_stage1 = np.array([network.genotype for network in network_history_stage1])
+        #     _, I = np.unique(genotype_history_stage1, axis=0, return_index=True)
+        # network_history_stage1 = np.array(network_history_stage1)[I]
+        # score_history_stage1 = np.array(score_history_stage1)[I]
+        #
+        # ## Sort
+        # I = np.flip(np.argsort(score_history_stage1))
+        # network_history_stage1 = network_history_stage1[I]
 
         # Stage 2: Training-based Search
         ## Get top-k best solutions in terms of training-free metric value. They are the input of SH.
-        topK_found_solutions = network_history_stage1[:self.n_candidate]
-
+        # topK_found_solutions = network_history_stage1[:self.n_candidate]
+        topK_found_solutions = []
+        result_ls = p.load(open(f'{root}/databases/result_local_search.p', 'rb'))
+        for genotype in result_ls[0]:
+            network = Network()
+            network.genotype = genotype
+            topK_found_solutions.append(network)
         print('- Stage 2: Successive Halving')
         ## Initialize Successive Halving
         optimizer_stage2 = SuccessiveHalving()
